fb00956df985fe4f46bb57506f41ce27674f2d2b
tboumadj <93995176+tboumadj@users.noreply.github.com>
2022-12-30 19:52:27 +0100

    Add files via upload

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,28 @@
 /*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 16:52:01 by tboumadj          #+#    #+#             */
-/*   Updated: 2022/12/30 16:52:03 by tboumadj         ###   ########.fr       */
+/*   Updated: 2022/12/30 19:50:25 by tboumadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <ctype.h>
+
+int main(int argc, char **argv) 
+{
+    if (argc > 1)
+    {
+        int i = 0;
+        int j;
+        while (++i < argc)
+        {
+            j = 0;
+            while(argv[i][j])
+                    std::cout << (char)toupper(argv[i][j++]);
+        }
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
+    return 0;
+}
