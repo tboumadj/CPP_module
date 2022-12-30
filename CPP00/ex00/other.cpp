879255eeebcccba8f3a27cd357201824d9405f5f
tboumadj <93995176+tboumadj@users.noreply.github.com>
2022-12-30 18:05:36 +0100

    Add files via upload

--- CPP00/ex00/other.cpp
+++ CPP00/ex00/other.cpp
@@ -6,16 +6,32 @@
 /*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 17:14:31 by tboumadj          #+#    #+#             */
-/*   Updated: 2022/12/30 17:15:29 by tboumadj         ###   ########.fr       */
+/*   Updated: 2022/12/30 18:04:34 by tboumadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int main() 
+int main(int argc, char **argv) 
 {
-    cout << "Hello, World!";
+    if (argc > 1)
+    {
+        int i = 1;
+        int j = 0;
+        while (i < argc)
+        {
+            char *tmp = argv[i];
+            while(tmp[j])
+                    putchar(toupper(tmp[j++]));
+            i++;
+            j = 0;
+        }
+    }
+    else
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    cout << endl;
     return 0;
 }
