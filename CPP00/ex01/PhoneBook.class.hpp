3ce97ddf99e4869cccca4f0fff2768960137cc10
tboumadj <93995176+tboumadj@users.noreply.github.com>
2022-12-31 18:54:49 +0100

    Add files via upload

--- CPP00/ex01/PhoneBook.class.hpp
+++ CPP00/ex01/PhoneBook.class.hpp
@@ -1,28 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   testcin.cpp                                        :+:      :+:    :+:   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/31 17:21:15 by tboumadj          #+#    #+#             */
-/*   Updated: 2022/12/31 18:54:23 by tboumadj         ###   ########.fr       */
+/*   Created: 2022/12/31 17:14:07 by tboumadj          #+#    #+#             */
+/*   Updated: 2022/12/31 18:35:25 by tboumadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef	PHONEBOOK_CLASS_H
+# define	PHONEBOOK_CLASS_H
 #include <iostream>
+#include <string>
 
-struct Foo {
-    int n;
-    Foo() {
-       std::cout << "Enter n: "; // no flush needed
-       std::cin >> n;
-    }
+class PhoneBook {
+
+public :
+
+	PhoneBook(void);
+	~PhoneBook(void);
+	void	print(void);
+
+private :
+
+	std::string name;
+
 };
 
-Foo f; // static object
-
-int main()
-{
-    std::cout << "f.n is " << f.n << '\n';
-}
+#endif
--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -6,7 +6,39 @@
 /*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 17:14:09 by tboumadj          #+#    #+#             */
-/*   Updated: 2022/12/31 17:14:11 by tboumadj         ###   ########.fr       */
+/*   Updated: 2022/12/31 18:53:57 by tboumadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.class.hpp"
+
+PhoneBook::PhoneBook(void) {
+
+	std::cout << "Constructor called" << std::endl;
+	return ;
+}
+
+PhoneBook::~PhoneBook(void) {
+
+	std::cout  << "Destructor called" << std::endl;
+	return ;
+}
+
+void    PhoneBook::print(void)
+{
+    std::cout << "Enter name: ";
+    getline(std::cin, this->name);
+    if (name.empty())
+        std::cout << "Void name !" << std::endl;
+    else
+        std::cout << "name = " << name << std::endl;
+
+    return ;
+}
+
+int main()
+{
+    PhoneBook i;
+    i.print();
+    return 0;
+}
