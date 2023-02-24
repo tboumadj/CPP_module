582f2c2b951eaa5d6fe2eb7ef53a2b8cc2aa0e23
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-02-24 19:21:15 +0100

    Add files via upload

---
+++ CPP00/ex01/PhoneBook.class.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/31 17:14:07 by tboumadj          #+#    #+#             */
+/*   Updated: 2023/01/11 11:59:13 by tboumadj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PHONEBOOK_CLASS_H
+# define	PHONEBOOK_CLASS_H
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <stdlib.h>
+#include "Contact.class.hpp"
+
+class PhoneBook {
+
+public :
+
+	PhoneBook(void);
+	~PhoneBook(void);
+
+	void			entry_road(void);
+	void			print_contact(void);
+
+private :
+
+	int				len;
+	int				count;
+	std::string		data;
+	Contact 		cnt[8];
+	void			welcome(void);
+};
+
+#endif
