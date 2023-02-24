d32691df3830f58112d0901d664daf355334c960
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-02-24 21:01:11 +0100

    Add files via upload

---
+++ CPP01/ex01/Zombie.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/24 20:57:06 by tboumadj          #+#    #+#             */
+/*   Updated: 2023/02/24 20:59:32 by tboumadj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <stdlib.h>
+
+Zombie*	zombieHorde( int N, std::string name );
+
+#endif
