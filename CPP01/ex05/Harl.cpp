eba4d59905ef2043a10a3aa7b09adc8950c806cb
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-03-08 13:48:08 +0100

    Add files via upload

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -1,1 +1,44 @@
 #include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	std::cout << "*Constructor of Harl called!*" << std::endl;
+	return ;
+}
+
+Harl::~Harl(void)
+{
+	std::cout << "*Destructor of Harl called!*" << std::endl;
+	return ;
+}
+
+//PUBLIC
+
+void	Harl::complain(std::string level)
+{
+	return ;
+}
+
+//PRIVATE
+
+void	Harl::debug(void)
+{
+	return ;
+}
+
+void	Harl::info(void)
+{
+	return ;
+}
+
+void	Harl::warning(void)
+{
+	return ;
+}
+
+void	Harl::error(void)
+{
+	return ;
+}
+
+
--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -5,4 +5,21 @@
 #include <cstdlib>
 #include <stdlib.h>
 
+class	Harl
+{
+public:
+
+	Harl(void);
+	~Harl(void);
+
+	void	complain(std::string level);
+
+private:
+
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+};
+
 #endif
