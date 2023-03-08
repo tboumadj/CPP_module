3cf2f04bc5fcbb49e2bb599d3664c33967b89e55
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-03-08 14:50:02 +0100

    Add files via upload

---
+++ CPP02/ex00/Fixed.cpp
@@ -0,0 +1,15 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	std::cout << "*Constructor of Fixed called!*" << std::endl;
+	return ;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "*Destructor of Fixed called!*" << std::endl;
+	return ;
+}
+
+
---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <stdlib.h>
+
+class	Fixed
+{
+
+public:
+
+	Fixed(void);
+	~Fixed(void);
+
+private:
+
+};
+
+#endif
