a55b4c5b13af00d3314a68e3788f467b0b9253c2
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-03-08 14:32:24 +0100

    Add files via upload

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -12,33 +12,46 @@ Harl::~Harl(void)
 	return ;
 }
 
-//PUBLIC
-
-void	Harl::complain(std::string level)
-{
-	return ;
-}
-
 //PRIVATE
 
 void	Harl::debug(void)
 {
+	std::cout << "*DEBUG* I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do !" << std::endl;
 	return ;
 }
 
 void	Harl::info(void)
 {
+	std::cout << "*INFO* I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger ! If you did, I wouldn't be asking for more !" << std::endl;
 	return ;
 }
 
 void	Harl::warning(void)
 {
+	std ::cout << "*WARNING* I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month. " << std::endl;
 	return ;
 }
 
 void	Harl::error(void)
 {
+	std::cout << "*ERROR* This is unacceptable ! I want to speak to the manager now." << std::endl;
 	return ;
 }
 
+//PUBLIC
+
+void	Harl::complain(std::string level)
+{
+	std::string count[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+   	void	(Harl::*f[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int i = 0;
+
+	while (i < 4)
+	{
+		if (count[i] == level)
+			(this->*f[i])();
+		i++;
+	}
+	return ;
+}
 
--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
+	Harl	h;
+	h.complain("DEBUG");
+	h.complain("INFO");
+	h.complain("WARNING");
+	h.complain("ERROR");
 	return (0);
 }
 
