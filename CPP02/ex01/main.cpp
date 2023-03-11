7fc6a30ac4d7869aa2f8cb5147d40dbceb1ef978
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-03-11 19:13:21 +0100

    Add files via upload

--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tboumadj@student.42mulhouse.fr <tboumadj>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 19:11:45 by tboumadj@student  #+#    #+#             */
-/*   Updated: 2023/03/11 18:01:05 by tboumadj         ###   ########.fr       */
+/*   Updated: 2023/03/11 19:12:54 by tboumadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 int main()
 {
+  Fixed z;
+  std::cout << "Empty fixed test" << std::endl;
 	return (0);
 }
