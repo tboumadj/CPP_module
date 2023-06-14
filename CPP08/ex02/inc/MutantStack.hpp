3f2251156edb5d199a8a9c6a50c81f97b7787b81
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-06-14 21:00:53 +0200

    Add files via upload

--- CPP08/ex02/inc/MutantStack.hpp
+++ CPP08/ex02/inc/MutantStack.hpp
@@ -16,4 +16,36 @@
 # include <ostream>
 # include <stack>
 
+template<typename T>
+class MutantStack: public std::stack<T>
+{
+  public:
+    MutantStack(): std::stack<T>()
+    {
+      std::cout << "Constructor Default MutantStack called!" << std::endl;
+      return ;
+    }
+    ~MutantStack()
+    {
+      std::cout << "Destructor MutantStack called!" << std::endl;
+    }
+    MutantStack(MutantStack const &co): std::stack<T>()
+    {
+      *this = co;
+    }
+    MutantStack &operator=(MutantStack const &co)
+    {
+      std::stack<T>::operator=(co);
+      return (*this);
+    }
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    iterator    begin()
+    {
+      return (this->c.begin());
+    }
+    iterator   end()
+    {
+      return (this->c.end());
+    }
+};
 #endif
