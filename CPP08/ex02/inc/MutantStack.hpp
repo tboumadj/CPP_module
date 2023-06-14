f67f57d6826907f22cb7a8d9e454ccc4d9744a1c
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-06-14 19:28:39 +0200

    Add files via upload

---
+++ CPP08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 18:02:59 by tboumadj          #+#    #+#             */
+/*   Updated: 2023/06/14 19:27:53 by tboumadj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+# include <iostream>
+# include <ostream>
+# include <stack>
+
+#endif
---
+++ CPP08/ex02/src/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 18:03:32 by tboumadj          #+#    #+#             */
+/*   Updated: 2023/06/14 19:25:47 by tboumadj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/MutantStack.hpp"
+
+int main()
+{
+  MutantStack<int> mstack;
+  mstack.push(5);
+  mstack.push(17);
+  std::cout << mstack.top() << std::endl;
+  mstack.pop();
+  std::cout << mstack.size() << std::endl;
+  mstack.push(3);
+  mstack.push(5);
+  mstack.push(737);
+//[...]
+  mstack.push(0);
+  MutantStack<int>::iterator it = mstack.begin();
+  MutantStack<int>::iterator ite = mstack.end();
+  ++it;
+  --it;
+  while (it != ite)
+  {
+  std::cout << *it << std::endl;
+  ++it;
+  }
+  std::stack<int> s(mstack);
+return 0;
+}
