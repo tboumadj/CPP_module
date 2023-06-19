db645e1b7ad6f9b7c60f212e16f70601df62f012
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-06-19 15:32:23 +0200

    Add files via upload

--- CPP09/ex00/src/main.cpp
+++ CPP09/ex00/src/main.cpp
@@ -12,7 +12,41 @@
 
 #include "../inc/BitcoinExchange.hpp"
 
-int main()
+void  extractData(const std::string &file_i)
 {
+  std::vector<data> dataa;
+  std::ifstream file(file_i.c_str());
+
+  if (!file)
+  {
+    std::cerr << "Error with opening file." << std::endl;
+    return ; //data
+  }
+
+  std::string line;
+  while (std::getline(file, line))
+  {
+    std::istringstream ss(line);
+    std::string champ;
+    data dataa;
+
+    while (std:getline(ss, champ, '|'))
+    {
+      data.champ1 = champ;
+    }
+  }
+}
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    std::cout << "input file paramater missing..." << std::endl;
+    return (0);
+  }
+  else
+  {
+    extractData(argv[1]);
+  }
   return (0);
 }
